1c9fff8d4b1a69fe98e77ec9a87719449af6c929
ohnoesganj <sunho3456@naver.com>
2025-02-13 16:17:01 +0900

    schema.d.ts type 사용

--- frontend/src/app/post/list/page.tsx
+++ frontend/src/app/post/list/page.tsx
@@ -1,3 +1,7 @@
+import { components } from "@/lib/backend/apiV1/schema";
+type PostDto = components["schemas"]["PostDto"];
+type PostItemPageDto = components["schemas"]["PageDto"];
+
 export default async function Page() {
   const response = await fetch("http://localhost:8080/api/v1/posts");
 
@@ -6,18 +10,34 @@ export default async function Page() {
   }
 
   const rsData = await response.json();
-
-  console.log(rsData.code);
-  console.log(rsData.msg);
-  console.log(rsData.data);
+  const pageDto: PostItemPageDto = rsData.data;
 
   return (
     <div>
       <h1>글 목록</h1>
+
+      <div>응답 코드: {rsData.code}</div>
+      <div>결과 메세지: {rsData.msg}</div>
+
+      <div>totalPages : {pageDto.totalPages}</div>
+      <div>totalItems : {pageDto.totalItems}</div>
+      <div>currentPageNo : {pageDto.currentPageNo}</div>
+      <div>pageSize : {pageDto.pageSize}</div>
+
       <hr />
       <ul>
-        <li>글1</li>
-        <li>글2</li>
+        {pageDto.items?.map((item: PostDto) => {
+          return (
+            <li className="border-2 border-red-500 my-2 p-2" key={item.id}>
+              <div>id : {item.id}</div>
+              <div>title : {item.title}</div>
+              <div>authorId : {item.authorId}</div>
+              <div>authorName : {item.authorName}</div>
+              <div>published : {`${item.published}`}</div>
+              <div>listed : {`${item.listed}`}</div>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
