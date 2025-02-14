050b7db78ae05887f57246fb3988bd9e9abe1054
ohnoesganj <sunho3456@naver.com>
2025-02-14 10:30:54 +0900

    UI 렌더링

---
+++ frontend/src/app/post/list/ClientPage.tsx
@@ -0,0 +1,100 @@
+"use client";
+
+import { components } from "@/lib/backend/apiV1/schema";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+
+export default function ClinetPage({
+  rsData,
+  keywordType,
+  keyword,
+  pageSize,
+  page,
+}: {
+  rsData: components["schemas"]["RsDataPageDto"];
+  keywordType?: "title" | "content";
+  keyword: string;
+  pageSize: number;
+  page: number;
+}) {
+  const router = useRouter();
+  const pageDto = rsData.data;
+  return (
+    <div>
+      <h1>글 목록</h1>
+      <div>응답 코드 : {rsData.code}</div>
+      <div>결과 메시지 : {rsData.msg}</div>
+      <div>totalPages : {pageDto.totalPages}</div>
+      <div>totalItems : {pageDto.totalItems}</div>
+      <div>currentPageNo : {pageDto.currentPageNo}</div>
+      <div>pageSize : {pageDto.pageSize}</div>
+      <hr />
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          const formData = new FormData(e.target as HTMLFormElement);
+          const searchKeyword = formData.get("keyword") as string;
+          const searchKeywordType = formData.get("keywordType") as string;
+          const page = 1;
+          const pageSize = formData.get("pageSize") as string;
+          router.push(
+            `/post/list?keywordType=${searchKeywordType}&keyword=${searchKeyword}&pageSize=${pageSize}&page=${page}`
+          );
+        }}
+      >
+        <select name="keywordType" defaultValue={keywordType}>
+          <option value="title">제목</option>
+          <option value="content">내용</option>
+        </select>
+        <input
+          placeholder="검색어 입력"
+          type="text"
+          name="keyword"
+          defaultValue={keyword}
+        />
+        <input type="submit" value="검색" />
+        <label className="ml-5" htmlFor="">
+          페이지당 행 개수 :
+        </label>
+        <select name="pageSize" defaultValue={pageSize}>
+          <option value="10">10</option>
+          <option value="30">30</option>
+          <option value="50">50</option>
+        </select>
+      </form>
+      <div className="flex gap-3">
+        {Array.from({ length: pageDto.totalPages }, (_, i) => i + 1).map(
+          (pageNo) => {
+            return (
+              <Link
+                key={pageNo}
+                className={pageNo == page ? `text-red-500` : `text-blue-500`}
+                href={`/post/list?keywordType=${keywordType}&keyword=${keyword}&pageSize=${pageSize}&page=${pageNo}`}
+              >
+                {pageNo}
+              </Link>
+            );
+          }
+        )}
+      </div>
+      <ul>
+        {pageDto.items.map((item) => {
+          return (
+            <li className="border-2 border-red-500 my-2 p-2" key={item.id}>
+              <div>id : {item.id}</div>
+              <div>title : {item.title}</div>
+              <div>authorId : {item.authorId}</div>
+              <div>authorName : {item.authorName}</div>
+              <div>published : {`${item.published}`}</div>
+              <div>listed : {`${item.listed}`}</div>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
+
+function userRouter() {
+  throw new Error("Function not implemented.");
+}
--- frontend/src/app/post/list/page.tsx
+++ frontend/src/app/post/list/page.tsx
@@ -1,6 +1,8 @@
 import { paths } from "@/lib/backend/apiV1/schema";
-import Link from "next/link";
 import createClient from "openapi-fetch";
+import Link from "next/link";
+
+import ClinetPage from "./ClientPage";
 
 const client = createClient<paths>({
   baseUrl: "http://localhost:8080",
@@ -35,66 +37,14 @@ export default async function Page({
   });
 
   const rsData = response.data!!;
-  const pageDto = rsData.data;
 
   return (
-    <div>
-      <h1>글 목록</h1>
-
-      <div>응답 코드: {rsData.code}</div>
-      <div>결과 메세지: {rsData.msg}</div>
-
-      <div>totalPages : {pageDto.totalPages}</div>
-      <div>totalItems : {pageDto.totalItems}</div>
-      <div>currentPageNo : {pageDto.currentPageNo}</div>
-      <div>pageSize : {pageDto.pageSize}</div>
-
-      <hr />
-
-      <form>
-        <select name="keywordType" defaultValue={keywordType}>
-          <option value="title">제목</option>
-          <option value="content">내용</option>
-        </select>
-        <input
-          placeholder="검색어 입력"
-          type="text"
-          name="keyword"
-          defaultValue={keyword}
-        />
-        <input type="submit" value="검색" />
-        <label className="ml-5" htmlFor="">
-          페이지당 행 개수 :
-        </label>
-        <select name="pageSize">
-          <option value="10">10</option>
-          <option value="30">30</option>
-          <option value="50">50</option>
-        </select>
-      </form>
-
-      <div className="flex gap-3">
-        {Array.from({ length: pageDto.totalPages }, (_, i) => i + 1).map(
-          (page) => {
-            return <Link href={`/post/list?page=${page}`}>{page}</Link>;
-          }
-        )}
-      </div>
-
-      <ul>
-        {pageDto.items.map((item) => {
-          return (
-            <li className="border-2 border-red-500 my-2 p-2" key={item.id}>
-              <div>id : {item.id}</div>
-              <div>title : {item.title}</div>
-              <div>authorId : {item.authorId}</div>
-              <div>authorName : {item.authorName}</div>
-              <div>published : {`${item.published}`}</div>
-              <div>listed : {`${item.listed}`}</div>
-            </li>
-          );
-        })}
-      </ul>
-    </div>
+    <ClinetPage
+      rsData={rsData}
+      pageSize={pageSize}
+      keyword={keyword}
+      keywordType={keywordType}
+      page={page}
+    />
   );
 }
