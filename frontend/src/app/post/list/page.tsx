4422de8cbe0d9289f10bbe4593f863363192f5de
ohnoesganj <sunho3456@naver.com>
2025-02-14 09:24:39 +0900

    글 목록 페이징 처리

--- frontend/src/app/post/list/page.tsx
+++ frontend/src/app/post/list/page.tsx
@@ -1,4 +1,5 @@
 import { paths } from "@/lib/backend/apiV1/schema";
+import Link from "next/link";
 import createClient from "openapi-fetch";
 
 const client = createClient<paths>({
@@ -11,15 +12,24 @@ export default async function Page({
   searchParams: {
     keywordType?: "title" | "content";
     keyword: string;
+    pageSize: number;
+    page: number;
   };
 }) {
-  const { keywordType = "title", keyword = "" } = await searchParams;
+  const {
+    keywordType = "title",
+    keyword = "",
+    pageSize = 10,
+    page = 1,
+  } = await searchParams;
 
   const response = await client.GET("/api/v1/posts", {
     params: {
       query: {
         keyword,
         keywordType,
+        pageSize,
+        page,
       },
     },
   });
@@ -53,7 +63,23 @@ export default async function Page({
           defaultValue={keyword}
         />
         <input type="submit" value="검색" />
+        <label className="ml-5" htmlFor="">
+          페이지당 행 개수 :
+        </label>
+        <select name="pageSize">
+          <option value="10">10</option>
+          <option value="30">30</option>
+          <option value="50">50</option>
+        </select>
       </form>
+
+      <div className="flex gap-3">
+        {Array.from({ length: pageDto.totalPages }, (_, i) => i + 1).map(
+          (page) => {
+            return <Link href={`/post/list?page=${page}`}>{page}</Link>;
+          }
+        )}
+      </div>
 
       <ul>
         {pageDto.items.map((item) => {
