4e84491a2fb2ba4e98a1e7470dc966828a91d629
ohnoesganj <sunho3456@naver.com>
2025-02-17 12:28:06 +0900

    스프링쿠키 to 브라우저

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,7 +1,21 @@
-import type { NextRequest } from "next/server";
+import { cookies } from "next/headers";
+import { NextResponse, type NextRequest } from "next/server";
+import client from "./lib/backend/client";
 
-export function middleware(request: NextRequest) {
-  console.log("middleware");
+export async function middleware(request: NextRequest) {
+  const reqToken = request.cookies.get("accessToken");
+  const nextResponse = NextResponse.next();
+
+  const response = await client.GET("/api/v1/members/me", {
+    headers: {
+      cookie: (await cookies()).toString(),
+    },
+  });
+
+  const springCookie = response.response.headers.getSetCookie();
+  nextResponse.headers.set("set-cookie", String(springCookie));
+
+  return nextResponse;
 }
 
 export const config = {
