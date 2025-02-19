c0be990cadf51695ac0d365b6a1e712679c245d2
ohnoesganj <sunho3456@naver.com>
2025-02-19 10:01:35 +0900

    button test

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Page() {
-  return <div>메인페이지입니다.</div>;
+  return (
+    <>
+      <div>메인페이지입니다.</div>
+      <Button className="bg-red-300 w-[100px]">Button</Button>
+    </>
+  );
 }
