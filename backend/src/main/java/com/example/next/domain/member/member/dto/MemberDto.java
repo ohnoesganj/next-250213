57f281cad7a2767b363538850d66fd5fa956491b
ohnoesganj <sunho3456@naver.com>
2025-02-19 09:40:16 +0900

    awesome font test

--- backend/src/main/java/com/example/next/domain/member/member/dto/MemberDto.java
+++ backend/src/main/java/com/example/next/domain/member/member/dto/MemberDto.java
@@ -12,17 +12,10 @@ public class MemberDto {
     @NonNull
     private long id;
     @NonNull
-    private LocalDateTime createdDate;
-    @NonNull
-    private LocalDateTime modifiedDate;
-    @NonNull
     private String nickname;
 
     public MemberDto(Member member) {
         this.id = member.getId();
-        this.createdDate = member.getCreatedDate();
-        this.modifiedDate = member.getModifiedDate();
-
         this.nickname = member.getNickname();
     }
 }
--- frontend/src/app/ClientLayout.tsx
+++ frontend/src/app/ClientLayout.tsx
@@ -2,20 +2,32 @@
 
 import { components } from "@/lib/backend/apiV1/schema";
 import client from "@/lib/backend/client";
+import { faThumbsUp } from "@fortawesome/free-regular-svg-icons";
+import { faBookBookmark } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
 
 export default function ClinetLayout({
   children,
   me,
+  fontVariable,
+  fontClassName,
 }: Readonly<{
   children: React.ReactNode;
   me: components["schemas"]["MemberDto"];
+  fontVariable: string;
+  fontClassName: string;
 }>) {
   const isLogined = me.id !== 0;
 
   return (
-    <html lang="en">
-      <body className="min-h-[100dvh] flex flex-col">
+    <html lang="en" className={`${fontVariable}`}>
+      <body className="min-h-[100dvh] flex flex-col ${fontClassName}">
+        <FontAwesomeIcon
+          icon={faThumbsUp}
+          className="fa-fw text-4xl text-[red]"
+        />
+        <FontAwesomeIcon icon={faBookBookmark} />
         <header className="flex gap-3">
           <Link href="/">메인</Link>
           <Link href="/about">소개</Link>
